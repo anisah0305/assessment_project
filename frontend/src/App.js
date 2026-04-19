03c98ec2660a30bf33c2ffcee494ee56d5da6714
Komllesh03 <komlleshbhaskar@gmail.com>
2026-04-19 21:51:39 +0800

    Add SubmitAssignment route to App component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Dashboard from "./pages/Dashboard";
+import SubmitAssignment from './pages/SubmitAssignment';
 import "./App.css";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/submit" element={<SubmitAssignment />} />
       </Routes>
     </Router>
   );
